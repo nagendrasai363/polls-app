4cfd456197889035776a0ff8bf76d5dbb72f36d9
nagendrasai363 <nagendrasai363@gmail.com>
2024-04-05 00:21:05 -0400

    Customizing the admin form

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -12,8 +13,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+            boolean = True,
+            ordering = "pub_date",
+            description = "was_published_recently"
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
